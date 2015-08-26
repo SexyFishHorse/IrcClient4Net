358e570f6ad6705d4efe23c2524d449bbc60ca75
Asbjørn Hansen <asser-dk@outlook.com>
2015-08-26 18:35:53 +0200

    throw an exception if the expected command is missing

--- IrcClient4Net/Validators/ResponseValidator.cs
+++ IrcClient4Net/Validators/ResponseValidator.cs
@@ -1,11 +1,17 @@
 ﻿namespace SexyFishHorse.Irc.Client.Validators
 {
+    using System;
     using SexyFishHorse.Irc.Client.Models;
 
     public class ResponseValidator : IResponseValidator
     {
         public void ValidateCommand(IrcMessage message, string expectedCommand)
         {
+            if (string.IsNullOrWhiteSpace(expectedCommand))
+            {
+                throw new ArgumentException("Expected command is not set", "expectedCommand");
+            }
+
             if (message == null)
             {
                 throw new ResponseValidationException("Did not receive a response from the server.");
