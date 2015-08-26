160f2c9a1794b6f902f94958d83729e34ebb0f3c
Asbjørn Hansen <asser-dk@outlook.com>
2015-08-26 18:39:34 +0200

    fixed the documentation for the response validator

--- IrcClient4Net/Validators/IResponseValidator.cs
+++ IrcClient4Net/Validators/IResponseValidator.cs
@@ -9,6 +9,7 @@
         /// </summary>
         /// <param name="message">The message to validate</param>
         /// <param name="expectedCommand">The command the message is expected to have</param>
+        /// <returns>The <see cref="IrcMessage"/>.</returns>
         IrcMessage ValidateCommand(IrcMessage message, string expectedCommand);
     }
 }
