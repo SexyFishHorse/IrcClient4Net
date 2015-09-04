7ff46d4a055a86952ac363452f319c5eef85bf3b
Asbjørn Hansen <aha@trustpilot.com>
2015-09-04 11:10:07 +0200

    moved client and streams to its own class

---
+++ IrcClient4Net/Models/ISocket.cs
@@ -0,0 +1,15 @@
+﻿namespace SexyFishHorse.Irc.Client.Models
+{
+    using System;
+
+    public interface ISocket : IDisposable
+    {
+        void WriteLine(string line);
+
+        string ReadLine();
+
+        void Flush();
+
+        void WriteLineAndFlush(string message);
+    }
+}
